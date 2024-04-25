a292306de63f2a8fa6cdc8466abb61e86a4bd09a
VladGBRL <86017579+VladGBRL@users.noreply.github.com>
2024-04-25 22:11:56 +0300

    Add files via upload

---
+++ Homework3_3/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main{
+
+    public static void main(String[] args) {
+        List<Shape> shapesList = new ArrayList<>();
+
+        shapesList.add(new Circle(2));
+        shapesList.add(new Rectangle(1, 3));
+        shapesList.add(new Circle(1));
+
+        double totalArea = calculateTotalArea(shapesList);
+
+        System.out.println("Total Area: " + totalArea);
+    }
+
+    public static double calculateTotalArea(List<Shape> shapes) {
+        double totalArea = 0;
+
+        for (Shape shape : shapes) {
+            totalArea += shape.getArea();
+        }
+
+        return totalArea;
+    }
+}
